cc28d51c08f3bee265f295d02373a5bb7fffe9d5
Lucas Antunes <lucasantuneslara06@gmail.com>
2024-11-19 06:46:12 -0300

    first commit

---
+++ service/app/Http/Controllers/VideoController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVideoRequest;
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller {
+    public function index() {
+        $videos = Video::all();
+        return $videos;
+    }
+    public function show($id) {
+        $video = Video::find($id);
+        return $video;
+    }
+    public function store(StoreVideoRequest $video) {
+        $path = $video->file('url')->store('videos', 'public');
+
+        if (!$path) {
+            return response()->json(['message' => 'Erro ao armazenar o vídeo.'], 500);
+        }
+
+        $newVideo = Video::create([
+            'name' => $video->input('name'),
+            'url' => $path,
+            'tags' => $video->input('tags'),
+            'isPrivate' => $video->input('isPrivate'),
+        ]);
+
+        if ($newVideo) {
+            return response()->json([
+                "message" => 'Novo vídeo adicionado com sucesso!',
+                'video' => $newVideo,
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Deu ruim. Te vira aí pra descobrir o que aconteceu.'
+        ], 422);
+    }
+    public function update() {
+    }
+    public function destroy() {
+    }
+}
---
+++ service/database/migrations/2024_11_13_195303_create_videos_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('url', 255);
+            $table->string('tags')->nullable();
+            $table->$table->boolean('isPrivate')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('videos');
+    }
+};

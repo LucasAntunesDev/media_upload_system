c73275c92ae858b5a2adb32555dc6b269d8a5370
Lucas Antunes <lucasantuneslara06@gmail.com>
2024-12-05 08:28:32 -0300

    fix: cors

--- service/app/Http/Middleware/Cors.php
+++ service/app/Http/Middleware/Cors.php
@@ -12,7 +12,7 @@ class Cors {
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization');
+        $response->headers->set('Access-Control-Allow-Headers', '*');
 
         if ($request->isMethod('OPTIONS')) {
             $response->setStatusCode(200);
